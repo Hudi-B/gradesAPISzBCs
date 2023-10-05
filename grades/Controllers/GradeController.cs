85cb22e081fc76135e3eefeb0caeb5e913f45aea
Hudi-B <vanosskanoss@gmail.com>
2023-10-05 08:14:57 +0200

    Created GetDb not tested

--- grades/Controllers/GradeController.cs
+++ grades/Controllers/GradeController.cs
@@ -2,12 +2,46 @@
 using System;
 using grades.Models;
 using static grades.Controllers.DTO;
+using MySql.Data.MySqlClient;
 
 namespace grades.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class GradeController : Controller
     {
         Connect connect = new();
         private readonly List<GradeDto> grades = new();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<GradeDto>> GetDb()
+        {
+            try
+            {
+                connect.connection.Open();
+
+                string sql = "SELECT * FROM `grades`";
+                MySqlCommand cmd = new MySqlCommand(sql, connect.connection);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                foreach (var data in reader)
+                {
+                    var result = new GradeDto(
+                        reader.GetGuid(0),
+                        reader.GetInt32(1),
+                        reader.GetString(2),
+                        reader.GetString(3)
+                        );
+                    grades.Add(result);
+                }
+
+                connect.connection.Close();
+
+                return StatusCode(200, grades);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(503, e);
+            }
+        }
     }
 }
